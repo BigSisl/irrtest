198f65c0c9e348cf5f1dd661aa98895ab2347172
bigz94 <lucien.zuercher@hotmail.com>
2012-09-04 14:19:35 +0200

    testing spark & irrlicht

---
+++ CMakeFiles/base.dir/DependInfo.cmake
@@ -0,0 +1,69 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "C:/cproject/irrtest/src/Core/SPK_BufferHandler.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_BufferHandler.cpp.obj"
+  "C:/cproject/irrtest/src/Core/SPK_DEF.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_DEF.cpp.obj"
+  "C:/cproject/irrtest/src/Core/SPK_Emitter.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_Emitter.cpp.obj"
+  "C:/cproject/irrtest/src/Core/SPK_Factory.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_Factory.cpp.obj"
+  "C:/cproject/irrtest/src/Core/SPK_Group.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_Group.cpp.obj"
+  "C:/cproject/irrtest/src/Core/SPK_Interpolator.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_Interpolator.cpp.obj"
+  "C:/cproject/irrtest/src/Core/SPK_Model.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_Model.cpp.obj"
+  "C:/cproject/irrtest/src/Core/SPK_Modifier.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_Modifier.cpp.obj"
+  "C:/cproject/irrtest/src/Core/SPK_Particle.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_Particle.cpp.obj"
+  "C:/cproject/irrtest/src/Core/SPK_Registerable.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_Registerable.cpp.obj"
+  "C:/cproject/irrtest/src/Core/SPK_Renderer.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_Renderer.cpp.obj"
+  "C:/cproject/irrtest/src/Core/SPK_System.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_System.cpp.obj"
+  "C:/cproject/irrtest/src/Core/SPK_Transformable.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_Transformable.cpp.obj"
+  "C:/cproject/irrtest/src/Core/SPK_Vector3D.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_Vector3D.cpp.obj"
+  "C:/cproject/irrtest/src/Core/SPK_Zone.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Core/SPK_Zone.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Emitters/SPK_NormalEmitter.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Emitters/SPK_NormalEmitter.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Emitters/SPK_RandomEmitter.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Emitters/SPK_RandomEmitter.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Emitters/SPK_SphericEmitter.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Emitters/SPK_SphericEmitter.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Emitters/SPK_StraightEmitter.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Emitters/SPK_StraightEmitter.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Modifiers/SPK_Collision.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Modifiers/SPK_Collision.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Modifiers/SPK_Destroyer.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Modifiers/SPK_Destroyer.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Modifiers/SPK_LinearForce.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Modifiers/SPK_LinearForce.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Modifiers/SPK_ModifierGroup.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Modifiers/SPK_ModifierGroup.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Modifiers/SPK_Obstacle.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Modifiers/SPK_Obstacle.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Modifiers/SPK_PointMass.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Modifiers/SPK_PointMass.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Modifiers/SPK_Vortex.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Modifiers/SPK_Vortex.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Renderers/SPK_Oriented3DRendererInterface.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Renderers/SPK_Oriented3DRendererInterface.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Renderers/SPK_QuadRendererInterface.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Renderers/SPK_QuadRendererInterface.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Zones/SPK_AABox.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Zones/SPK_AABox.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Zones/SPK_Cylinder.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Zones/SPK_Cylinder.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Zones/SPK_Line.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Zones/SPK_Line.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Zones/SPK_Plane.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Zones/SPK_Plane.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Zones/SPK_Point.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Zones/SPK_Point.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Zones/SPK_Ring.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Zones/SPK_Ring.cpp.obj"
+  "C:/cproject/irrtest/src/Extensions/Zones/SPK_Sphere.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/Extensions/Zones/SPK_Sphere.cpp.obj"
+  "C:/cproject/irrtest/src/RenderingAPIs/Irrlicht/SPK_IRRBuffer.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/RenderingAPIs/Irrlicht/SPK_IRRBuffer.cpp.obj"
+  "C:/cproject/irrtest/src/RenderingAPIs/Irrlicht/SPK_IRRLineRenderer.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/RenderingAPIs/Irrlicht/SPK_IRRLineRenderer.cpp.obj"
+  "C:/cproject/irrtest/src/RenderingAPIs/Irrlicht/SPK_IRRPointRenderer.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/RenderingAPIs/Irrlicht/SPK_IRRPointRenderer.cpp.obj"
+  "C:/cproject/irrtest/src/RenderingAPIs/Irrlicht/SPK_IRRQuadRenderer.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/RenderingAPIs/Irrlicht/SPK_IRRQuadRenderer.cpp.obj"
+  "C:/cproject/irrtest/src/RenderingAPIs/Irrlicht/SPK_IRRRenderer.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/RenderingAPIs/Irrlicht/SPK_IRRRenderer.cpp.obj"
+  "C:/cproject/irrtest/src/RenderingAPIs/Irrlicht/SPK_IRRSystem.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/RenderingAPIs/Irrlicht/SPK_IRRSystem.cpp.obj"
+  "C:/cproject/irrtest/src/RenderingAPIs/OpenGL/SPK_GLExtHandler.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/RenderingAPIs/OpenGL/SPK_GLExtHandler.cpp.obj"
+  "C:/cproject/irrtest/src/RenderingAPIs/OpenGL/SPK_GLLineRenderer.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/RenderingAPIs/OpenGL/SPK_GLLineRenderer.cpp.obj"
+  "C:/cproject/irrtest/src/RenderingAPIs/OpenGL/SPK_GLLineTrailRenderer.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/RenderingAPIs/OpenGL/SPK_GLLineTrailRenderer.cpp.obj"
+  "C:/cproject/irrtest/src/RenderingAPIs/OpenGL/SPK_GLPointRenderer.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/RenderingAPIs/OpenGL/SPK_GLPointRenderer.cpp.obj"
+  "C:/cproject/irrtest/src/RenderingAPIs/OpenGL/SPK_GLQuadRenderer.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/RenderingAPIs/OpenGL/SPK_GLQuadRenderer.cpp.obj"
+  "C:/cproject/irrtest/src/RenderingAPIs/OpenGL/SPK_GLRenderer.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/RenderingAPIs/OpenGL/SPK_GLRenderer.cpp.obj"
+  "C:/cproject/irrtest/src/SPK_All.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/SPK_All.cpp.obj"
+  "C:/cproject/irrtest/src/SPK_GL_All.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/SPK_GL_All.cpp.obj"
+  "C:/cproject/irrtest/src/SPK_IRR_ALL.cpp" "C:/cproject/irrtest/CMakeFiles/base.dir/src/SPK_IRR_ALL.cpp.obj"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/metoritewars.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/metoritewars.dir/src/main.cpp.obj"
+  "bin/metoritewars.pdb"
+  "bin/metoritewars.exe"
+  "bin/metoritewars.exe.manifest"
+  "bin/libmetoritewars.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/metoritewars.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
